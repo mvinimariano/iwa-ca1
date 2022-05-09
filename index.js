57c7097ddb869f44368d9723c0d009330be7ad2b
mvinimariano <mvinimariano@hotmail.com>
2022-05-09 16:03:42 +0000

    gitpod.yml, dockerfile,morgan, route.js, MongoDb

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const   http = require("http"),
         logger = require("morgan"),
         express = require("express"),
-        bodyParser = require("body-parser");
+        bodyParser = require("body-parser"),
+        mongoose = require("mongoose");
 
         let app = express();
         let port = 8000;
 
-        app.use(require('./routes'));
+
         app.use(bodyParser.json());
+        app.use(require('./routes'));
         app.use(logger("tiny"));
+
+        mongoose.connect('mongodb://localhost/test');
+
+mongoose.connection.on('error', (err) => { 
+    console.log('Mongodb Error: ', err); 
+    process.exit();
+});
+mongoose.connection.on('connected', () => { 
+    console.log('MongoDB is successfully connected');
+});
+
 
 
 
